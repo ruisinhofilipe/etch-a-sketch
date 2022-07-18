65f2ddace4a99235f332db10e03c2c672e7229f3
Rui Silva <ruisinhofilipe@gmail.com>
2022-07-18 23:16:27 +0200

    Final Version

--- script.js
+++ script.js
@@ -1,82 +1,5 @@
 let mainContainer = document.querySelector("#container");
-
-//New measures button and Rainbow button
-function button(){
-
-    // New measures button creation
-    const button = document.createElement("button");
-    button.classList.add("btn");
-    button.textContent = "New grid";
-    bottomContainer.appendChild(button);
-
-    // Rainbow button creation
-    const rainbowButton = document.createElement("button");
-    rainbowButton.classList.add("rainbow");
-    rainbowButton.textContent = "Rainbow mode";
-    bottomContainer.appendChild(rainbowButton);
-
-    // Default color button creation
-    const defaultButton = document.createElement("button");
-    defaultButton.classList.add("defaultColor");
-    defaultButton.textContent = "Normal mode";
-    bottomContainer.appendChild(defaultButton);
-
-
-    // Restart button creation
-    const restartButton = document.createElement("button");
-    restartButton.classList.add("restartButton");
-    restartButton.textContent = "Restart grid";
-    bottomContainer.appendChild(restartButton);
-    
-
-    // New measures button
-    button.addEventListener("click", () => {
-        let row = document.querySelectorAll(".rows");
-        row.forEach(e => e.remove());
-        let column = document.querySelectorAll(".columns");
-        column.forEach(e => e.remove());
-        let i;
-        do{
-           i = prompt("Number of cells (between 2-100): ");
-        }while(i < 2 || i > 100);
-        let numberCells = i;
-        createGrid(numberCells);
-    }); 
-
-    // Rainbow button 
-    rainbowButton.addEventListener("click", () => {
-        let square = document.querySelectorAll(".rows");
-        square.forEach(squar => {
-            squar.addEventListener("mouseover", () => {
-                squar.style.backgroundColor = randomColor();
-            });
-        });
-    });
-
-    // Default color button
-    defaultButton.addEventListener("click", () => {
-        let square = document.querySelectorAll(".rows");
-        square.forEach(squar => {
-            squar.addEventListener("mouseover", () => {
-                squar.style.backgroundColor = "#a8adad";
-            });
-        });
-    });
-
-    // Restart button 
-    restartButton.addEventListener("click", () => {
-        location.reload();
-    }); 
-}
-
-function randomColor(){
-    let elements = "0123456789ABCDEF";
-    let color = "#";
-    for(let i = 0; i < 6; i++){
-        color += elements[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+let color = "#a8adad";
 
 //Grid container creation
 let gridContainer = document.createElement("div");
@@ -90,12 +13,23 @@ function createGrid(size){
             for (let r = 0; r < size; r++){
                 let rows = document.createElement("div");
                 columns.appendChild(rows).classList.add("rows");
-            }
-    }
+            };
+    };
+    
     let square = document.querySelectorAll(".rows");
     square.forEach(squar => {
         squar.addEventListener("mouseover", () => {
-            squar.style.backgroundColor = "#a8adad";
+            if(color === "#a8adad"){
+                squar.style.backgroundColor = color;
+                console.log("arroz");
+            }else if(color === "normal"){
+                squar.style.backgroundColor = "red";
+                console.log("massa");
+            }else if(color === "erase"){
+                squar.style.backgroundColor = "#7fe3f5"
+            }else{
+                squar.style.backgroundColor = randomColor();
+            }
         });
     });
 }
@@ -104,8 +38,88 @@ const bottomContainer = document.createElement("div");
 bottomContainer.classList.add("bottomContainer");
 container.appendChild(bottomContainer);
 
-button();
 createGrid(16);
+
+//New measures button creation
+const button = document.createElement("button");
+button.classList.add("btn");
+button.textContent = "New grid";
+bottomContainer.appendChild(button);
+
+//Rainbow button creation
+const rainbowButton = document.createElement("button");
+rainbowButton.classList.add("rainbow");
+rainbowButton.textContent = "Rainbow mode";
+bottomContainer.appendChild(rainbowButton);
+
+//Default color button creation
+const defaultButton = document.createElement("button");
+defaultButton.classList.add("defaultColor");
+defaultButton.textContent = "Normal mode";
+bottomContainer.appendChild(defaultButton);
+
+//Erase button creation
+const eraseButton = document.createElement("button");
+eraseButton.classList.add("erase");
+eraseButton.textContent = "Erase";
+bottomContainer.appendChild(eraseButton);
+
+//Restart button creation
+const restartButton = document.createElement("button");
+restartButton.classList.add("restartButton");
+restartButton.textContent = "Restart grid";
+bottomContainer.appendChild(restartButton);
+
+
+//New measures button
+button.addEventListener("click", () => {
+    let row = document.querySelectorAll(".rows");
+    row.forEach(e => e.remove());
+    let column = document.querySelectorAll(".columns");
+    column.forEach(e => e.remove());
+    let i;
+    do{
+       i = prompt("Number of cells (between 2-100): ");
+    }while(i < 2 || i > 100);
+    let numberCells = i;
+    createGrid(numberCells);
+}); 
+
+//Rainbow button 
+rainbowButton.addEventListener("click", () => {
+    color = "rainbow";
+});
+
+//Default color button
+defaultButton.addEventListener("click", () => {
+    color = "normal";
+});
+
+//Erase button
+eraseButton.addEventListener("click", () => {
+    color = "erase";
+});
+
+//Restart button 
+restartButton.addEventListener("click", () => {
+    let square = document.querySelectorAll(".rows");
+    square.forEach(squar => {
+        squar.style.backgroundColor = "#7fe3f5";
+    })
+}); 
+
+function randomColor(){
+    let elements = "0123456789ABCDEF";
+    let color = "#";
+    for(let i = 0; i < 6; i++){
+        color += elements[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+
+
 
 
 
